47760927d78169012945003d695e264fcbed32b5
Matt Glenn <highopenended@gmail.com>
2025-01-17 15:33:25 -0500

    Initial commit

---
+++ random_spell_generator/src/firebase-config.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  // Your Firebase configuration will go here
+  // You'll get this from the Firebase Console
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app; 
